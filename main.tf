b2019d375a4d002721aae53cfbca9f3405a0910e
Adeyemi Ayodipe <Ayodipeadeyemi@gmail.com>
2023-05-03 15:34:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,15 @@ terraform {
       version = "~> 4.25.0"
     }
   }
+    backend "s3"{
+    bucket = "s3bucket2029"
+    key = "demo.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
   # Configuration options
-  region = "us-east-1"
+    region = "us-east-1"
+
 }
