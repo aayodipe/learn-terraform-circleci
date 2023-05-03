67d3584c683c15fc073965152408e41cb50c46fc
Adeyemi Ayodipe <Ayodipeadeyemi@gmail.com>
2023-05-03 15:30:54 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,10 @@
-resource "aws_instance" "web" {
-  ami           = var.ami_id
-  instance_type = var.instance_type_id
-
+resource "aws_instance" "main" {
+  ami           = "ami-001089eb624938d9f"
+  instance_type = "t2.micro"
+  count         = 2
+  
   tags = {
-    Name = var.tag_name
+    Name = "IaC-Test"
+    Env = "dev"
   }
 }
